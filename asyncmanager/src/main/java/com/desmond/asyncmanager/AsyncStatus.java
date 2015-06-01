83a2c3cdb357184460c9bc862a50b0c089f61250
desmond <desmond@replaid.com>
2015-06-01 16:35:15 +0800

    Added intDef annotations in AsyncStatus

--- asyncmanager/src/main/java/com/desmond/asyncmanager/AsyncStatus.java
+++ asyncmanager/src/main/java/com/desmond/asyncmanager/AsyncStatus.java
@@ -1,51 +1,64 @@
 package com.desmond.asyncmanager;
 
+import android.support.annotation.IntDef;
+
 /**
  * State of a TaskRunnable during its entire lifecycle in the thread.
  */
 public class AsyncStatus {
 
+    @IntDef({STATE_ACTION_CANCELLED, STATE_ACTION_WAITING, STATE_ACTION_STARTED, STATE_ACTION_COMPLETED})
+    public @interface AsyncStatusState {}
     public static final int STATE_ACTION_CANCELLED = -1;
     public static final int STATE_ACTION_WAITING = 0;
     public static final int STATE_ACTION_STARTED = 1;
     public static final int STATE_ACTION_COMPLETED = 2;
 
-    private int mStatus;
+    private int mState;
 
     public AsyncStatus() {
-        mStatus = STATE_ACTION_WAITING;
+        setStatus(STATE_ACTION_WAITING);
     }
 
     boolean isWaiting() {
-        return mStatus == STATE_ACTION_WAITING;
+        return getAsyncStatusState() == STATE_ACTION_WAITING;
     }
 
     boolean isStarted() {
-        return mStatus == STATE_ACTION_STARTED;
+        return getAsyncStatusState() == STATE_ACTION_STARTED;
     }
 
     boolean isCompleted() {
-        return mStatus == STATE_ACTION_COMPLETED;
+        return getAsyncStatusState() == STATE_ACTION_COMPLETED;
     }
 
     boolean isCancelled() {
-        return mStatus == STATE_ACTION_CANCELLED;
+        return getAsyncStatusState() == STATE_ACTION_CANCELLED;
     }
 
     void completed() {
-        mStatus = STATE_ACTION_COMPLETED;
+        setStatus(STATE_ACTION_COMPLETED);
     }
 
     void started() {
-        mStatus = STATE_ACTION_STARTED;
+        setStatus(STATE_ACTION_STARTED);
     }
 
     void waiting() {
-        mStatus = STATE_ACTION_WAITING;
+        setStatus(STATE_ACTION_WAITING);
     }
 
     void cancelled() {
-        mStatus = STATE_ACTION_CANCELLED;
+        setStatus(STATE_ACTION_CANCELLED);
+    }
+
+    protected void setStatus(@AsyncStatusState int state) {
+        mState = state;
+    }
+
+    @AsyncStatusState
+    protected int getAsyncStatusState() {
+        return mState;
     }
 
     boolean isCleanable() {
@@ -53,7 +66,7 @@ public class AsyncStatus {
     }
 
     String getMessage() {
-        switch (mStatus) {
+        switch (mState) {
             case STATE_ACTION_CANCELLED:
                 return "Cancelled";
 
