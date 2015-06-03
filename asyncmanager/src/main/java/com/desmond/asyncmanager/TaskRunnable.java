bcc6f6f513c96b74f5b142eff785a045c4a5416f
boxme <desmond.ng.yang.yi@gmail>
2015-06-03 10:21:25 +0800

    added annotations

--- asyncmanager/src/main/java/com/desmond/asyncmanager/TaskRunnable.java
+++ asyncmanager/src/main/java/com/desmond/asyncmanager/TaskRunnable.java
@@ -1,6 +1,9 @@
 package com.desmond.asyncmanager;
 
+import android.support.annotation.CallSuper;
+import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
+import android.support.annotation.WorkerThread;
 
 import java.lang.ref.WeakReference;
 
@@ -89,6 +92,7 @@ public abstract class TaskRunnable<Params, Result, ResultHandler> implements Run
         }
     }
 
+    @WorkerThread
     public abstract Result doLongOperation(Params params) throws InterruptedException;
 
     public void callback(Result result) {}
@@ -104,6 +108,7 @@ public abstract class TaskRunnable<Params, Result, ResultHandler> implements Run
         }
     }
 
+    @MainThread
     private void callbackOnUIThread() {
         synchronized (this) {
             if (isActive()) {
